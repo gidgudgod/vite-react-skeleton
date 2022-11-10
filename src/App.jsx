847d7e5adae032a474111adff20bc3b9f4499d13
gideon_ks <106825924+gidgudgod@users.noreply.github.com>
2022-11-10 16:53:34 +0700

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import CardTravel from "./components/CardTravel";
+import data from "./data/data.js";
 
 function App() {
-  return <div className="App"></div>;
+  const cardTravels = data.map(function (item) {
+    return (
+      <div className="wrapper">
+        <CardTravel key={item.id} {...item} />
+        <hr></hr>
+      </div>
+    );
+  });
+  console.log(cardTravels);
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="travel-list">{cardTravels}</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav>
+      <img src="/image/airplane-fill.svg" alt="logo-icon" />
+      <h1>my travel journal.</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
