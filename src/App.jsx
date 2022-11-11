250493be08503e193351d01a7aa92c2c6bdc7715
gideon_ks <106825924+gidgudgod@users.noreply.github.com>
2022-11-11 23:43:21 +0700

    Revert "Initial Commit"
    This reverts commit 6f81a305bb2f70104530d83a90b42aa8ee844376.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import "./App.css";
-import Header from "./components/Header";
-import Meme from "./components/Meme";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Meme />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
