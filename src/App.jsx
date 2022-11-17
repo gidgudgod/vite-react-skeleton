e367b40be0999822d961b8f9bcd82f86180cef55
gideon_ks <106825924+gidgudgod@users.noreply.github.com>
2022-11-17 19:39:21 +0700

    Fix tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-xl font-bold text-fuchsia-600">Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
